0adf508a2a397aea1a2b7f039c8a90acda8213bf
NotFunnyRight <1981nawin@gmail.com>
2024-12-14 14:32:45 +0700

    Final... Maybe

--- Assets/Script/Charge.cs
+++ Assets/Script/Charge.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 public class Charge : MonoBehaviour
 {
     public float Speed = 4.5f;
-    private float isRight;
-    public Boss1Movement Boss;
+    public Boss1Movement Boss;  // Reference to Boss's movement script
+    private Vector3 direction;  // Fireball's movement direction
 
     private void Start()
     {
-        if(Boss.isTurnRight)
+        // Initially, set the direction based on Boss's facing direction
+        if (Boss.isTurnRight)
         {
-            isRight = 1f;
+            direction = Vector3.right;  // Fireball moves right if the boss is facing right
         }
         else
         {
-            isRight = -1f;
+            direction = Vector3.left;   // Fireball moves left if the boss is facing left
         }
     }
+
     void Update()
     {
-        transform.position += (isRight * transform.right) * Time.deltaTime * Speed;
+        // Update the direction based on the boss's current facing
+        if (Boss.isTurnRight)
+        {
+            direction = Vector3.right;  // Fireball goes right
+        }
+        else
+        {
+            direction = Vector3.left;   // Fireball goes left
+        }
+
+        // Move the fireball in the determined direction
+        transform.position += direction * Speed * Time.deltaTime;
     }
 }
